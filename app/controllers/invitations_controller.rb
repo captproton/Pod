d3b459a910d387365195bc415962dc3e61b7eb06
Ocean Shore <oceanshore@TiVoHelp.gateway.2wire.net>
2009-10-02 13:03:06 -0700

    Mod'ed layout to show invitation links

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -10,9 +10,9 @@ class InvitationsController < ApplicationController
       if current_user
         Mailer.deliver_invitation(@invitation, signup_url(@invitation.token))
         flash[:notice] = "Thank you, invitation sent."
-        redirect_to root_url        
+        redirect_to account_url        
       else
-        flash[:notice] = "Thank you, we will notify you when we are ready."
+        flash[:notice] = "Thank you.  We will get back to you within 24 hours."
         redirect_to root_url                
       end
     else
