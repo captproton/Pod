39a6a4967861e70aa5b147ee6847a0eb2deb4330
Ocean Shore <oceanshore@TiVoHelp.gateway.2wire.net>
2009-10-01 23:22:08 -0700

    improved layout for root

--- app/controllers/frontdoor_controller.rb
+++ app/controllers/frontdoor_controller.rb
@@ -1,6 +1,10 @@
 class FrontdoorController < ApplicationController
   
   def index
+    @invitation = Invitation.new
+    @user_session = UserSession.new
+    
+    render(:layout => false) 
   end
 
 end
