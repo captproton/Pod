3f36e66daf9145ff7cdbcbb06190ee9ff1b487ee
Ocean Shore <oceanshore@captproton.local>
2009-10-02 20:07:32 -0700

    Gmail: Aha! Closer, this Son of Eldorado had production element missing.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,3 +19,21 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+
+config.action_view.cache_template_loading = true
+config.action_mailer.raise_delivery_errors = true
+
+
+if CONFIG['smtp_address']
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address => CONFIG['smtp_address'],
+    :port => CONFIG['smtp_port'], 
+    :domain => CONFIG['smtp_domain'],
+    :user_name => CONFIG['smtp_user_name'],
+    :password => CONFIG['smtp_password'],
+    :authentication => CONFIG['smtp_authentication'].to_sym,
+    :enable_starttls_auto => CONFIG['smtp_enable_starttls_auto']
+  }
+end
+
