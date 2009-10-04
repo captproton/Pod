788d34a0c3832c7fd8d6fb5975921d7600a1a036
Carl Tanner <carl@captproton.com>
2009-10-04 13:54:35 -0700

    For mass-assignment, and so I could set Profile in a User form, I added :full_name to :attr_accessible in Profile.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,4 @@
 class Profile < ActiveRecord::Base
   belongs_to :user, :class_name => "User", :foreign_key => "user_id"
-  attr_accessible :more_info_url, :bio, :location, :language, :time_zone
+  attr_accessible :more_info_url, :bio, :location, :language, :time_zone, :full_name
 end
